3588e7ed1ed91c1d04af6a60c9864ab5be248def
Movlan Aliyev <movlan@live.com>
2023-01-27 19:34:56 -0800

    jdbc commit

---
+++ src/test/resources/Queries/day01/select.sql
@@ -0,0 +1,37 @@
+/*
+ This is multi line comments
+    MAC users
+    - Click over query
+    - CMD+ENTER
+    Win users
+    - Click over query
+    - CTRL + ENTER
+    Make sure you choosed line that you need to run
+ */
+-- DONT USE CLASS-CHAT TO SHARE SOLUTIONS
+
+select * from employees;
+-- Single line comments
+-- SQL language is NOT CASE SENSITIVE
+
+select * FROM eMpLoYeeS;
+
+-- display all information from departments table
+select * from departments;
+
+-- display firstname from employees table
+select first_name from employees;
+
+select FIRST_NAME from employees;
+
+-- display firstname,lastname and salary from employees table
+select first_name,last_name,salary  from employees;
+
+-- display all information from locations table
+select * from locations;
+
+-- display country name and region id from countries table
+select country_name,region_id from countries;
+
+-- display job_id and job title from jobs table
+select job_id,job_title from jobs;
